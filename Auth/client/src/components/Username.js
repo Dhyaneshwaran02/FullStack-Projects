431a97e5474a9d2f08d53ea82e9986448c4be074
dhyaneshwaranK <2034015aiml@cit.edu.in>
2023-10-17 07:19:26 +0530

    login

--- Auth/client/src/components/Username.js
+++ Auth/client/src/components/Username.js
@@ -1,64 +1,33 @@
-import React, { useState } from 'react';
-import img1 from "../assets/ramakrishnan-nataraj-SzMR1Vcf5aw-unsplash.jpg"
+import React from 'react';
+import avatar from "../assets/avatar.jpg"
+import { Link } from 'react-router-dom';
+import styles from '../styles/Username.module.css';
 
 export default function Username() {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
-    <div className='bg-blue-200 min-h-screen flex justify-center items-center'>
-      <div className='container mx-auto flex w-4/5 h-96 bg-white shadow-lg rounded-lg overflow-hidden'>
-        {/* Left Column (Image) */}
-        <div className='w-1/2'>
-          <img src={img1} alt='avatar' className='w-full h-full object-cover' />
+    <div className="container mx-auto flex justify-center items-center h-screen">
+      <div className={`${styles.gclass}`}>
+        <div className='title flex flex-col items-center'>
+          <h4 className='text-4xl font-bold'>Hello Again!</h4>
+          <span className='py-2 text-lg text-center text-gray-500'>
+            Explore More by Connecting with Us.
+          </span>
         </div>
 
-        {/* Right Column (Username and Password) */}
-        <div className='w-1/2 p-6 flex flex-col justify-between'>
-          <div className='text-center'>
-            <h4 className='text-2xl font-semibold text-gray-800'>Hello! Welcome Back</h4>
-            <p className='text-gray-500'>Explore more by connecting with us.</p>
+        <form className='py-1'>
+          <div className='profile flex justify-center py-2'>
+            <img src={avatar} alt="avatar" className="max-w-32 max-h-32" />
           </div>
 
-          <form className='mt-6'>
-            {/* Username Input */}
-            <div className='mb-4'>
-              <input
-                type='text'
-                placeholder='Username'
-                className='w-3/4 px-3 py-2 flex justify-center items-center mx-auto border rounded-lg focus:outline-none focus:border-blue-500'
-              />
-            </div>
-            
-            {/* Password Input */}
-            <div className='mb-4 relative'>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                placeholder='Password'
-                className='w-3/4 px-3 py-2 flex justify-center items-center mx-auto border rounded-lg focus:outline-none focus:border-blue-500'
-              />
-              <span
-                className='absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer'
-                onClick={togglePasswordVisibility}
-              >
-                {showPassword ? '🙈' : '👁️'}
-              </span>
-            </div>
-          </form>
-
-          {/* Submit Button */}
-          <div className='text-center mt-4'>
-            <button
-              type='submit'
-              className='w-3/4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none'
-            >
-              Let's Go
-            </button>
+          <div className='textbox flex flex-col items-center gap-4'>
+            <input type="text" placeholder='username' />
+            <button type="submit">Let's Go</button>
           </div>
-        </div>
+
+          <div className='text-center py-2'>
+            <span className='text-gray-500'>Not a Member<Link className='text-red-500' to='/register'>Register Now</Link></span>
+          </div>
+        </form>
       </div>
     </div>
   );
